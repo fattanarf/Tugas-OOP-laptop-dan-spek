1f6203ca667737b6d5e2d65318e5b529a039faa5
fattanarf <fattanarf@gmail.com>
2025-01-15 15:10:39 +0700

    Add files via upload

---
+++ Tugas pertemuan 1_Bu Aul_OOP/listIdentitas.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class listIdentitas {
+
+    Scanner input = new Scanner(System.in);
+    String merek;
+    String warna;
+    String spek;
+
+    void tampilSpek(){
+        System.out.print("Masukan merek laptop kamu: ");
+        merek = input.nextLine();
+
+        System.out.print("Masukan warna laptop kamu: ");
+        warna = input.nextLine();
+
+        System.out.print("Masukan spek laptop kamu: ");
+        spek = input.nextLine();
+
+        System.out.println("===========================================================");
+        System.out.println("Laptop Fattan bermerek: " + merek);
+        System.out.println("karena saya suka " + warna + ", jadi saya pilij warna: " + warna);
+        System.out.println("dan ber spek: " + spek);
+    }
+
+}
---
+++ Tugas pertemuan 1_Bu Aul_OOP/objekLaptop.java
@@ -0,0 +1,8 @@
+public class objekLaptop {
+    public static void main(String[] args) {
+        
+        listIdentitas nama = new listIdentitas();
+        nama.tampilSpek();
+    }
+    
+}
